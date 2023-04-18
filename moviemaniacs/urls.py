5478a6800fe40d2fe5f0c1ba4ff78e040420baec
Josiah Charley <josiah.charley@student.awesomeinc.org>
2023-04-18 01:11:09 +0000

    made changes to views/added imports

--- moviemaniacs/urls.py
+++ moviemaniacs/urls.py
@@ -5,12 +5,10 @@ from .views import *
 from rest_framework import routers
 
 urlpatterns = [
-    ...,
     path('', include(router.urls)),
-    
     path('user/signup/', UserCreate.as_view(), name="create_user"),
     path('users/<int:pk>/', UserDetail.as_view(), name="get_user_details"),
-
+    path('user/login/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),
     path('token/obtain/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),  # override sjwt stock token
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- moviemaniacs/views.py
+++ moviemaniacs/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import status, permissions, generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import action
+from .models import CustomUser
 
 class UserCreate(APIView):
     permission_classes = (permissions.AllowAny,)
