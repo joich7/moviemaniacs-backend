5d181b2c63ca5064d031de3b8b3d5fd24113e368
Josiah Charley <josiah.charley@student.awesomeinc.org>
2023-04-18 00:48:00 +0000

    edits to models, views

--- moviemaniacs/views.py
+++ moviemaniacs/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+class UserCreate(APIView):
+    permission_classes = (permissions.AllowAny,)
+    authentication_classes = ()
+
+    def post(self, request, format='json'):
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                json = serializer.data
+                return Response(json, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserDetail(generics.RetrieveAPIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
 # Create your views here.
